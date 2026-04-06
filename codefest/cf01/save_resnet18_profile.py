57aaa55c3e1f77be411444adededec1ef945e650
ganeshreddy-4149 <saiganes@pdx.edu>
2026-04-05 22:24:53 -0700

    Update save_resnet18_profile.py

--- codefest/cf01/save_resnet18_profile.py
+++ codefest/cf01/save_resnet18_profile.py
@@ -3,9 +3,15 @@ from torchinfo import summary
 
 model = models.resnet18()
 
-stats = summary(model, input_size=(1, 3, 224, 224), verbose=0)
+stats = summary(
+    model,
+    input_size=(1, 3, 224, 224),
+    depth=3,
+    col_names=("input_size", "output_size", "num_params", "mult_adds"),
+    verbose=0
+)
 
 with open("resnet18_profile.txt", "w", encoding="utf-8") as f:
     f.write(str(stats))
 
-print("Saved successfully!")
+print("ResNet-18 profile saved successfully!")
